34ccd5786b9b4ee7518e6402042a1bb3aa71df2a
JunseopPark <metternichch@gmail.com>
2019-09-13 14:49:59 +0900

    sdf
    sdf

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nomflix
+
+Learning React and ES6 by building a Movie Discovery App.
+
+## Screen
+
+- Home
+- TV Shows
+- Search
+- Detail
+  asds
---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import styles from "./Header.module.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <ul className={styles.navList}>
+          <li>
+            <a href="/">Movies</a>
+          </li>
+          <li>
+            <a href="/tv">TV</a>
+          </li>
+          <li>
+            <a href="/search">Search</a>
+          </li>
+        </ul>
+      </header>
+    );
+  }
+}
